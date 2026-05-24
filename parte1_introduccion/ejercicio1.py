6359359dba7b96be54b0fbbf62cc8e731c21268e
JOSE MENDOZA SANCHEZ <el_anime130@hotmail.com>
2026-05-24 12:14:43 -0500

    TOMA DICK

--- parte1_introduccion/ejercicio1.py
+++ parte1_introduccion/ejercicio1.py
@@ -5,7 +5,36 @@
 
 def main():
     print("--- EJERCICIO 1: OPERACIONES BÁSICAS ---")
-    # TODO: Desarrolla tu código aquí
+
+# Solicitar al usuario que ingrese dos números enteros 
+
+num1 = int(input("Ingrese el primer número entero: ")) 
+
+num2 = int(input("Ingrese el segundo número entero: ")) 
+
+ 
+
+# Calcular la suma, resta, multiplicación y división 
+
+suma = num1 + num2 
+
+resta = num1 - num2 
+
+multiplicacion = num1 * num2 
+
+division = num1 / num2 
+
+ 
+
+# Mostrar los resultados 
+
+print("Suma:", suma) 
+
+print("Resta:", resta) 
+
+print("Multiplicación:", multiplicacion) 
+
+print("División:", division) 
     
 if __name__ == "__main__":
     main()
--- parte2_estructurados/ejercicio6.py
+++ parte2_estructurados/ejercicio6.py
@@ -5,7 +5,22 @@
 
 def main():
     print("--- EJERCICIO 6: CADENAS DE TEXTO ---")
-    # TODO: Desarrolla tu código aquí
+
+#Desarrollar un programa que solicite una frase al usuario y muestre la cantidad de caracteres 
+
+frase = input("Ingrese una frase: ") 
+
+cantidad_caracteres = len(frase) 
+
+usuario_frase = f"La cantidad de caracteres en la frase es: {cantidad_caracteres}" 
+
+print(usuario_frase) 
+
+print(usuario_frase.upper()) 
+
+print(usuario_frase.lower()) 
+
+print(usuario_frase[::-1]) 
     
 if __name__ == "__main__":
     main()
